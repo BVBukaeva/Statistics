dca53fbcf2edb7be0fc2b2b85ff8cf66ae927b00
Bukaeva Bayrta <bbembeeva@gmail.com>
2021-03-21 21:37:19 +0300

    fix methods

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -10,17 +10,13 @@ public class StatsService {
     }
 
     public int average(int[] sales) {
-        int sum = 0;
-        for (int sale : sales) {
-            sum = sum + sale;
-        }
-        return sum / sales.length;
+        return sum(sales) / sales.length;
     }
 
     public int maxMonth(int[] sales) {
         int maxMonth = 0;
-        for (int i = 0, salesLength = sales.length; i < salesLength; i++) {
-            if (i != 0 && sales[maxMonth] < sales[i]) {
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[maxMonth] < sales[i]) {
                 maxMonth = i;
             }
 
@@ -40,14 +36,9 @@ public class StatsService {
     }
 
     public int countMonthLessThanAverage(int[] sales) {
-        int sum = 0;
-        for (int sale : sales) {
-            sum = sum + sale;
-        }
-        int average = sum / sales.length;
         int count = 0;
         for (int sale : sales) {
-            if (sale < average) {
+            if (sale < average(sales)) {
                 count++;
             }
         }
@@ -55,14 +46,9 @@ public class StatsService {
     }
 
     public int countMonthMoreThanAverage(int[] sales) {
-        int sum = 0;
-        for (int sale : sales) {
-            sum = sum + sale;
-        }
-        int average = sum / sales.length;
         int count = 0;
         for (int sale : sales) {
-            if (sale > average) {
+            if (sale > average(sales)) {
                 count++;
             }
         }
